0676e607515d5512cae7a842d852734206cf4f14
Francesca Garbagnati <f.g.garbagnati@gmail.com>
2017-02-09 13:28:15 -0800

    Adds init function

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+var GameState = {
+	init: function() {
+		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		this.scale.pageAlignHorizontally = true;
+		this.scale.pageAlignVertically = true;
+	},
+	
+}
