41b0bd5594864c6d383120203971819a617402c6
Francesca Garbagnati <f.g.garbagnati@gmail.com>
2017-02-09 13:32:50 -0800

    Preloads images

--- public/js/main.js
+++ public/js/main.js
@@ -4,5 +4,15 @@ var GameState = {
 		this.scale.pageAlignHorizontally = true;
 		this.scale.pageAlignVertically = true;
 	},
-	
+	preload: function() {
+		this.load.image('ground', 'assets/images/ground.png');
+		this.load.image('platform', 'assets/images/platform.png');
+		this.load.image('goal', 'assets/images/gorilla3.png');
+		this.load.image('arrowButton', 'assets/images/arrowButton.png');
+		this.load.image('actionButton', 'assets/images/actionButton.png');
+		this.load.image('barrel', 'assets/images/barrel.png');
+
+		this.load.spritesheet('player', 'assets/images/plater_spritesheet.png', 28, 30, 5, 1, 1)
+		this.load.spritesheet('fire', 'assets/images/fire_spritesheet.png', 20, 21, 2, 1, 1)
+	}
 }
