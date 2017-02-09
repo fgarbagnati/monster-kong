78c7dff680cd2247f73de64ed0bdd064747df44a
Francesca Garbagnati <f.g.garbagnati@gmail.com>
2017-02-09 13:35:49 -0800

    Adds create function, adds ground sprite, initializes game

--- public/js/main.js
+++ public/js/main.js
@@ -12,7 +12,17 @@ var GameState = {
 		this.load.image('actionButton', 'assets/images/actionButton.png');
 		this.load.image('barrel', 'assets/images/barrel.png');
 
-		this.load.spritesheet('player', 'assets/images/plater_spritesheet.png', 28, 30, 5, 1, 1)
+		this.load.spritesheet('player', 'assets/images/player_spritesheet.png', 28, 30, 5, 1, 1)
 		this.load.spritesheet('fire', 'assets/images/fire_spritesheet.png', 20, 21, 2, 1, 1)
+	},
+	create: function() {
+		this.ground = this.add.sprite(0, 500, 'ground');
+	},
+	update: function() {
+
 	}
-}
+};
+
+var game = new Phaser.Game(360, 592, Phaser.AUTO);
+game.state.add('GameState', GameState);
+game.state.start('GameState');
