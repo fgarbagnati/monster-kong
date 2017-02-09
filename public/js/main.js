f43e7fed2328050fd9053e4e5533144677b0a0f2
Francesca Garbagnati <f.g.garbagnati@gmail.com>
2017-02-09 13:45:19 -0800

    Enables physics and gravity

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,8 @@ var GameState = {
 		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 		this.scale.pageAlignHorizontally = true;
 		this.scale.pageAlignVertically = true;
+
+		this.game.physics.startSystem(Phaser.Physics.ARCADE);
 	},
 	preload: function() {
 		this.load.image('ground', 'assets/images/ground.png');
