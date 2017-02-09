948dca7ee9a29afa257db51bb76854e5dffda6bc
Francesca Garbagnati <f.g.garbagnati@gmail.com>
2017-02-09 13:38:37 -0800

    Adds platform, player, and player animation

--- public/js/main.js
+++ public/js/main.js
@@ -17,6 +17,13 @@ var GameState = {
 	},
 	create: function() {
 		this.ground = this.add.sprite(0, 500, 'ground');
+
+		var platform = this.add.sprite(0, 300, 'platform');
+
+		this.player = this.add.sprite(100, 200, 'player', 3);
+		this.player.anchor.setTo(0.5);
+		this.player.animations.add('walking', [0, 1, 2, 1], 6, true);
+		this.player.play('walking');
 	},
 	update: function() {
 
