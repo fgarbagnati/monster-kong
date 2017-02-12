e9a00143b37638d735ef133ba5d7f6ce0987a22e
Francesca Garbagnati <f.g.garbagnati@gmail.com>
2017-02-12 15:05:36 -0800

    Makes platform group, makes them immovable, allows gravity and enables collision

--- public/js/main.js
+++ public/js/main.js
@@ -28,10 +28,23 @@ var GameState = {
 		this.ground.body.allowGravity = false;
 		this.ground.body.immovable = true;
 
-		this.platform = this.add.sprite(0, 300, 'platform');
-		this.game.physics.arcade.enable(this.platform);
-		this.platform.body.allowGravity = false;
-		this.platform.body.immovable = true;
+		var platformData = [
+			{"x": 0, "y": 430},
+			{"x": 45, "y": 560},
+			{"x": 90, "y": 290},
+			{"x": 0, "y": 140}
+		];
+
+		this.platforms = this.add.group();
+		this.platforms.enableBody = true;
+
+		platformData.forEach(function(element) {
+			this.platforms.create(element.x, element.y, 'platform');
+		}, this);
+
+		this.platforms.setAll('body.immovable', true);
+		this.platforms.setAll('body.allowGravity', false);
+		this.platforms
 
 		this.player = this.add.sprite(100, 200, 'player', 3);
 		this.player.anchor.setTo(0.5);
@@ -43,7 +56,7 @@ var GameState = {
 	},
 	update: function() {
 		this.game.physics.arcade.collide(this.player, this.ground);
-		this.game.physics.arcade.collide(this.player, this.platform);
+		this.game.physics.arcade.collide(this.player, this.platforms);
 
 		this.player.body.velocity.x = 0;
 
